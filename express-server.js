7513575194f6945b04e83a47f72676db6b924e7b
Russel Mueller <bohjaangles@gmail.com>
2022-10-31 20:20:03 +0000

    added some basic code to setup server using express

---
+++ express-server.js
@@ -0,0 +1,17 @@
+// PRE CONFIG
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const urLDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
+});
+
+app.listen(port, () => {
+  console.log(`you are listening on port ${port}`);
+});
