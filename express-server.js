8c9097c8eafb87c1def9021d9f7674ad4a735429
Russel Mueller <bohjaangles@gmail.com>
2022-11-02 21:13:40 +0000

    added html template for registering to tinyapp, and made a get route for /register

--- express-server.js
+++ express-server.js
@@ -19,59 +19,64 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/register', (req, res) => {
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  return res.render('urls_register', templateVars);
+});
+
 app.post('/login', (req, res) => {
   // req.body is already an object here>>> {username: 'whateveristypedinthesubmissionbox'}
   res.cookie('username', req.body.username);
-  res.redirect('/urls');
+  return res.redirect('/urls');
 })
 
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
-  res.redirect('/urls');
+  return res.redirect('/urls');
 })
 
 app.get('/', (req, res) => {
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies['username'] }; 
-  res.render('urls_index', templateVars);
+  return res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
   const templateVars = { username: req.cookies['username'] };
-  res.render('urls_new', templateVars);
+  return res.render('urls_new', templateVars);
 });
 
 app.post('/urls', (req, res) => {
   let temp = generateRandomString();
   urlDatabase[temp] = req.body.longURL; 
-  res.redirect(`/urls/${temp}`); 
+  return res.redirect(`/urls/${temp}`); 
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username'] };
-  res.render("urls_show", templateVars);
+  return res.render("urls_show", templateVars);
 });
 
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id]; 
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.post('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL; 
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
 app.get(`/u/:id`, (req, res) => {
   const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  return res.redirect(longURL);
 });
 
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+  return res.json(urlDatabase);
 });
 
 
