0ced98da1019bd837c3f8141bffb9c2f2f0cd986
Russel Mueller <bohjaangles@gmail.com>
2022-11-03 21:31:49 +0000

    fixed the pathing for urldatabase as it is now obecjts inside of an object, and made a function to limit the urls visible on the /urls page to tose that are owned or tagged with the user id logged in

--- express-server.js
+++ express-server.js
@@ -7,7 +7,7 @@ const { signedCookie } = require('cookie-parser');
 const morgan = require('morgan');
 
 // 
-// MISC functions and essentials
+// ********   MISC functions and essentials
 //
 
 app.set('view engine', 'ejs');
@@ -24,7 +24,7 @@ const validateEmail = (targetEmail, usersObj) => {
     }
   }
   return false;
-}
+};
 
 const validatePW = (targetPW, usersObj) => {
   for (const user in usersObj) {
@@ -33,7 +33,7 @@ const validatePW = (targetPW, usersObj) => {
     }
   }
   return false;
-}
+};
 
 const getUserByEmail = (email, usersObj) => {
   for (const user in usersObj) {
@@ -42,15 +42,32 @@ const getUserByEmail = (email, usersObj) => {
     }
   }
   return;
-}
+};
+
+const urlsForUser = (userid, database) => {
+  let filterdDB = {};
+  for (const data in database) {
+    if (database[data].userID === userid){
+      filterdDB[data] = database[data]
+    }
+  }
+  return filterdDB;
+};
+
 
 //
-// DATABASE
+//                                     VVVVV--------> DATABASE(s) <--------VVVVV
 //
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: '1t55sw'
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: '1t55sw'
+  }
 };
 
 const users = {
@@ -62,45 +79,60 @@ const users = {
 };
 
 //
-// MIDDLEWARE
+//                                                >>>> MIDDLEWARE <<<<
 //
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //
-// ALL THE ROUTES
+//                                   ~~~~~~~~~~~~~ALL THE ROUTES~~~~~~~~~~~~~
 //
 // to acess the correct user, use the cookie! req.cookie gives obj like this { user_id: gibberish }
 //
 
 app.get('/register', (req, res) => {
-  const currentUser = users[req.cookies.user_id];
-  const templateVars = { urls: urlDatabase, user: currentUser };
-  return res.render('urls_register', templateVars);
+  if (req.cookies.user_id) {
+    return res.redirect('/urls');
+  }
+  return res.render('urls_register');
 });
 
 
 app.post('/register', (req, res) => {
   let randomID = generateRandomString();
-  
+
   if (req.body.email === '' || req.body.password === '') {
     res.send('404');
   }
-  
+
   if (validateEmail(req.body.email, users)) {
     res.send('404');
   }
 
   users[randomID] = { id: randomID, email: req.body.email, password: req.body.password };
-  // currentUser = users[req.cookie.user_id];
-  // const templateVars = { users: currentUser };
+
   res.cookie('user_id', randomID);
   return res.redirect('/urls');
 });
 
+///                ///////  REGISTER  \\\\\\\\\
+//                 ----------------------------
+///                \\\\\\\\  LOGIN   //////////
+
+
+app.get('/login', (req, res) => {
+  if (req.cookies.user_id) {
+    return res.redirect('/urls');
+  }
+
+  const currentUser = users[req.cookies.user_id];
+  let templateVars = { user: currentUser };
+  return res.render('login', templateVars);
+});
+
 app.post('/login', (req, res) => {
-  
+
   if (!validateEmail(req.body.email, users)) {
     res.send(' 403 ');
   }
@@ -108,15 +140,9 @@ app.post('/login', (req, res) => {
     res.send('403');
   }
   let userID = getUserByEmail(req.body.email, users);
-  
-  res.cookie('user_id', userID)  
-  return res.redirect('/urls');
-});
 
-app.get('/login', (req, res) => {
-  const currentUser = users[req.cookies.user_id]
-  let templateVars = {user: currentUser} 
-  return res.render('login', templateVars)
+  res.cookie('user_id', userID);
+  return res.redirect('/urls');
 });
 
 app.post('/logout', (req, res) => {
@@ -124,47 +150,72 @@ app.post('/logout', (req, res) => {
   return res.redirect('/login');
 });
 
-//  USER LOGIN / REGISTRATION ^^^
+// ======================================== USER LOGIN / REGISTRATION ^^^
 //
-// APP FUNCTIONALITY VVV
+// ======================================== APP FUNCTIONALITY VVV
 
 app.get('/', (req, res) => {
-  return res.redirect('/urls');
+  return res.redirect('/register');
 });
 
 app.get('/urls', (req, res) => {
   const currentUser = users[req.cookies.user_id];
-  const templateVars = { urls: urlDatabase, user: currentUser };
+  let filtered = urlsForUser(req.cookies.user_id, urlDatabase);
+  let templateVars = { urls: filtered, user: currentUser }; // replace database with filtered database
   // req.cookies - gives object value of the cookie(s)
   return res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
+  if (!req.cookies.user_id) {
+    return res.redirect('/login');
+  }
   const currentUser = users[req.cookies.user_id];
   const templateVars = { user: currentUser };
   return res.render('urls_new', templateVars);
 });
 
 app.post('/urls', (req, res) => {
-  let temp = generateRandomString();
-  urlDatabase[temp] = req.body.longURL;
-  return res.redirect(`/urls/${temp}`);
+
+  let randomID = generateRandomString();
+  if (req.cookies.user_id) {
+    urlDatabase[randomID] = {
+      longURL: req.body.longURL,
+      userID: req.cookies.user_id
+    };
+  }
+
+  // let temp = generateRandomString();
+  // urlDatabase[temp] = req.body.longURL;
+  return res.redirect(`/urls/${randomID}`);
 });
 
+// for get, use req.params / vs post uses req.body
 app.get("/urls/:id", (req, res) => {
   const currentUser = users[req.cookies.user_id];
-  const templateVars = { user: currentUser, longURL: urlDatabase[req.params.id] };
+  const templateVars = { user: currentUser, urlInfo: urlDatabase[req.params.id], shortUrlId: req.params.id };
+  // console.log('urlinfo', `${JSON.stringify(req.params.id)}`);
   return res.render("urls_show", templateVars);
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
+  if (req.cookies.user_id) {
+    delete urlDatabase[req.params.id];
+  }
   return res.redirect('/urls');
 });
 
 app.post('/urls/:id', (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  return res.redirect('/urls');
+  if (req.cookies.user_id) {
+    // urlDatabase[req.params.id] = {
+    //   longURL: req.body.longURL,
+    //   userID: req.cookies.user_id
+    return res.redirect('/urls');
+  }
+
+
+
+  return res.redirect('/register');
 });
 
 app.get(`/u/:id`, (req, res) => {
@@ -177,7 +228,7 @@ app.get('/urls.json', (req, res) => {
 });
 
 //
-// IT LISTENS...
+// .......................IT LISTENS...
 //
 
 app.listen(port, () => {
