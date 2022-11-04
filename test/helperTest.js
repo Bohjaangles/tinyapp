2d79ce9dcb1809e5d0c3b2b4d2d99dd8d335c034
Russel Mueller <bohjaangles@gmail.com>
2022-11-04 04:53:01 +0000

    just refactored a few mistakkes and removed some obesolete pieces of code that were hanging around not doing anything, added a catch all route for anything missed

---
+++ test/helperTest.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user, expectedUserID);
+  });
+  it('should return undefined if a non existant email is provided', function() {
+    const user = getUserByEmail("bad@example.com", testUsers);
+    assert.isUndefined(user);
+  });
+});
+
+
+// ./node_modules/mocha/bin/mocha"
